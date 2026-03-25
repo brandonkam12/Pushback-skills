fcb0ee1608f9fa969c2eaad7493beb4eb8e3c8fe
brandonkam12 <brandonkam12@gmail.com>
2026-03-24 20:28:51 -0600

    Update auton.cpp

--- skills(lemlib)/src/auton.cpp
+++ skills(lemlib)/src/auton.cpp
@@ -3,16 +3,16 @@
 #include "robot_config.hpp"
 #include <sys/_intsup.h>
 
-int leftwallx = 0;
+int leftwallx = 72;
 
-int leftwally = 0;
+int rightwallx = 0;
 
-void rightxreset() {
-	chassis.setPose(((leftwallx - 4.75 - distance_left.get_distance()) * 0.0393701), chassis.getPose().y, chassis.getPose().theta);
+void skillsrightxreset() {
+	chassis.setPose(((rightwallx - 4.75 - distance_left.get_distance()) * 0.0393701), chassis.getPose().y, chassis.getPose().theta);
 }
 
-void leftxreset() {
-	chassis.setPose(((leftwallx + 4.75 + distance_right.get_distance()) * 0.0393701), chassis.getPose().y, chassis.getPose().theta);
+void skillsleftxreset() {
+	chassis.setPose(((leftwallx + 4.75 + distance_left.get_distance()) * 0.0393701), chassis.getPose().y, chassis.getPose().theta);
 }
 
 void left_auton() {
@@ -32,7 +32,7 @@ void left_auton() {
 	chassis.turnToPoint(-30.132, 16.266, 700);
 	chassis.moveToPoint(-30.132, 16.266, 800);
 
-	leftxreset();
+	// leftxreset();
 
 	pros::delay(500);
 	
@@ -81,14 +81,14 @@ void right_auton() {
 	chassis.turnToPoint(30.132, 16.266, 700);
 	chassis.moveToPose(30.132, 16.266, -5, 800);
 
-	rightxreset();
+	// rightxreset();
 
 	pros::delay(500);
 
 	bar.set_value(true);
 	chassis.turnToPoint(30.132, 0.8, 700);
 	chassis.moveToPoint(30.132, 0.8, 800, {.maxSpeed = 50});//match loader
-	pros::delay(500);
+	pros::delay(1000);
 
 	chassis.moveToPose(29.332, 34.399, 175, 800, {.forwards = false, .maxSpeed = 63.5});//scoring
 	intake_move(12000);
@@ -116,17 +116,135 @@ void right_auton() {
 void skills_auton() {
     chassis.setPose({0, 0, 0});
 
-    outtake.move_voltage(-12000);
-    mouth.move_voltage(12000);
-    wing.set_value(true);
+	intake_move(12000);
+	stopper.set_value(false);
 
-    chassis.setPose({0, 0, 0});
-    backwards(6000, 250);
-    bar.set_value(true);
-    forward(7000, 1500);
-    bar.set_value(false);
-    backwards(7000, 600);
-}
+    // outtake.move_voltage(-12000);
+    // mouth.move_voltage(12000);
+    // wing.set_value(true);
+
+    // chassis.setPose({0, 0, 0});
+    // backwards(6000, 250);
+    // bar.set_value(true);
+    // forward(7000, 1500);
+    // bar.set_value(false);
+    // backwards(7000, 600);
+
+	chassis.moveToPoint(-17.066, 35.733, 600);
+
+	chassis.turnToPoint(-11.136, 41.429, 500, {.forwards = false});
+	chassis.moveToPoint(-11.136, 41.429, 800, {.forwards = false});//middle goal
+	scoremode_bool = false; //false is middle goal
+	midgoalswitch.set_value(false);
+	stopper.set_value(true);
+	intake_move(12000);
+	pros::delay(500);
+	scoremode_bool = true;
+	stopper.set_value(false);
+	midgoalswitch.set_value(true);
+
+	chassis.turnToPoint(-46.18, 9.833, 500);
+	chassis.moveToPoint(-46.18, 9.833, 800);
+
+	bar.set_value(true);
+	chassis.turnToPoint(-46.18, -7.402, 500);
+	chassis.moveToPoint(-46.18, -7.402, 800, {.maxSpeed = 50});//match loader
+
+	chassis.moveToPoint(-46.18, 9.833, 800, {.forwards = false});
+
+	bar.set_value(false);
+
+	chassis.turnToPoint(-61.865, 17.866, 500);
+	chassis.moveToPoint(-61.865, 17.866, 800);
+
+	chassis.turnToPoint(-62.531, 97.597, 500);
+	chassis.moveToPoint(-62.531, 97.597, 1000);//long 1
+
+	chassis.turnToPoint(-47.065, 97.064, 500);
+	chassis.moveToPoint(-47.065, 97.064, 800);
+
+	chassis.turnToPoint(-47.52, 83.175, 500, {.forwards = false});
+	chassis.moveToPoint(-47.52, 83.175, 800, {.forwards = false});//scoring 1
+
+	skillsleftxreset();
+
+	stopper.set_value(true);
+	pros::delay(500);
+	stopper.set_value(false);
+	bar.set_value(true);
+
+	chassis.moveToPoint(-46.946, 112.665, 800, {.maxSpeed = 50});//match loader 2
+	pros::delay(1000);
+	
+	chassis.moveToPoint(-47.065, 97.064, 700, {.forwards = false});
+	bar.set_value(false);
+
+	chassis.turnToPoint(0, 98.397, 500);
+	chassis.moveToPoint(0, 98.397, 800);
+
+	chassis.turnToPoint(0, 120.263, 500);
+	chassis.moveToPoint(0, 120.263, 1000, {.minSpeed = 20});//far park zone
+
+	chassis.moveToPoint(0, 85.597, 800, {.forwards = false});
+
+	chassis.turnToPoint(-25.199, 75.731, 500);
+	chassis.moveToPoint(-25.199, 75.731, 800);
+	chassis.waitUntil(20);
+	bar.set_value(true);
+	pros::delay(300);
+	bar.set_value(false);
+
+	chassis.turnToPoint(-10, 63.465, 500);
+	chassis.moveToPoint(-10, 63.465, 800);//middle bottom goal
+	intakelift.set_value(true);
+	intake_move(-6000);
+	pros::delay(700);
+
+	chassis.moveToPoint(-25.199, 75.731, 750, {.forwards = false});
+	intake_move(12000);
+
+	chassis.turnToPoint(24.666, 77.331, 500);
+	chassis.moveToPoint(24.666, 77.331, 800);//middle balls
+
+	chassis.turnToPoint(48.665, 93.597, 500);
+	chassis.moveToPoint(48.665, 93.597, 800);
+
+	bar.set_value(true);
+	chassis.turnToPoint(48.399, 118.396, 500);
+	chassis.moveToPoint(48.399, 118.396, 800, {.maxSpeed = 50});//match loader 3
+	pros::delay(500);
+
+	chassis.moveToPoint(48.665, 93.597, 800, {.forwards = false});
+	bar.set_value(false);
+
+	chassis.turnToPoint(62.265, 87.731, 500);
+	chassis.moveToPoint(62.265, 87.731, 800);
+
+	chassis.turnToPoint(62.265, 10.933, 500);
+	chassis.moveToPoint(62.265, 10.933, 1200);//long 2
+
+	chassis.turnToPoint(48.399, 10.933, 500);
+	chassis.moveToPoint(48.399, 10.933, 800);
+
+	chassis.turnToPoint(47.332, 26.399, 500, {.forwards = false});
+	chassis.moveToPoint(47.332, 26.399, 800, {.forwards = false});//scoring 2
+	stopper.set_value(true);
+	intake_move(12000);
+	pros::delay(1000);
+	stopper.set_value(false);
+	bar.set_value(true);
+	skillsrightxreset();
+
+	chassis.moveToPoint(48.132, -6.933, 800);//match loader
+	pros::delay(500);
+
+	chassis.moveToPoint(48.399, 4.8, 800, {.forwards = false});
+
+	chassis.turnToPoint(1.2, -10.666, 500);
+	chassis.moveToPoint(1.2, -10.666, 1000, {.minSpeed = 30});//park
+
+};
+
 void sawp_auton() {
 
     chassis.setPose({0, 0, 0});
